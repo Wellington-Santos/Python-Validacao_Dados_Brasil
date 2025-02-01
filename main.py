20ca5e00b6c013fdb09d20c7b13d3b1ddc16bc5e
Wellington-Santos <wellington.september@gmail.com>
2025-02-01 00:21:37 -0300

    Realizando uma reatoração de na validação de documentos

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from validacoes.validacao_cpf import Validacao_CPF
+from validacoes.validacao_cpf_cnpj import Documento
 from validate_docbr import CPF
 from validate_docbr import CNPJ
 
 exemplo_cnpj = "35379838000112"
 exemplo_cpf = "71455454036"
 
-documento = Validacao_CPF(exemplo_cnpj,"cnpj")
+documento = Documento.cria_documento(exemplo_cnpj)
 
 print(documento)
